89a487898de69bfcfa99039ff3170f8c8373b1dc
krishnakaru18 <krishnakaru212@gmail.com>
2024-11-25 10:44:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var a=10
+var b=20
+console.log(a+b)
+
+
+let c=10
+let d=20
+console.log(a+b)
+
+
+const e=10
+const f=20
+console.log(a+b)
+
+
+
+var obj={
+    name:"ndndn",
+    a:10,
+
+
+}
+console.log(obj)
